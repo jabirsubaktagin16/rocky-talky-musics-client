5a89ba018887254fcf17c2ba216935d49fb01735
Ahmad Subaktagin Jabir <subaktagin.jabir16@gmail.com>
2023-03-11 16:54:08 +0600

    Entry Screen Loading Removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -8,7 +7,6 @@ import Inventory from "./Pages/Inventory/Inventory";
 import AddNewItem from "./Pages/ManageInventory/AddNewItem/AddNewItem";
 import ManageInventory from "./Pages/ManageInventory/ManageInventory/ManageInventory";
 import MyItems from "./Pages/MyItems/MyItems";
-import Loading from "./Pages/Shared/Loading/Loading";
 import NotFound from "./Pages/Shared/NotFound/NotFound";
 import RequireAuth from "./Pages/SignIn/RequireAuth/RequireAuth";
 import SignIn from "./Pages/SignIn/SignIn/SignIn";
@@ -17,69 +15,52 @@ import SupplierDetails from "./Pages/Suppliers/SupplierDetails/SupplierDetails";
 import Suppliers from "./Pages/Suppliers/Suppliers/Suppliers";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  const [completed, setCompleted] = useState(undefined);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(true);
-      setTimeout(() => {
-        setCompleted(true);
-      }, 2000);
-    }, 250);
-  }, []);
   return (
-    <>
-      {!completed ? (
-        <>{loading && <Loading />}</>
-      ) : (
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/suppliers" element={<Suppliers />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/supplier/:id" element={<SupplierDetails />} />
-            <Route
-              path="/inventory/:id"
-              element={
-                <RequireAuth>
-                  <Inventory />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/manageInventory"
-              element={
-                <RequireAuth>
-                  <ManageInventory />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/addItem"
-              element={
-                <RequireAuth>
-                  <AddNewItem />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/myItems"
-              element={
-                <RequireAuth>
-                  <MyItems />
-                </RequireAuth>
-              }
-            />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/suppliers" element={<Suppliers />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/supplier/:id" element={<SupplierDetails />} />
+        <Route
+          path="/inventory/:id"
+          element={
+            <RequireAuth>
+              <Inventory />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/manageInventory"
+          element={
+            <RequireAuth>
+              <ManageInventory />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/addItem"
+          element={
+            <RequireAuth>
+              <AddNewItem />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/myItems"
+          element={
+            <RequireAuth>
+              <MyItems />
+            </RequireAuth>
+          }
+        />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
 
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <Toaster />
-        </div>
-      )}
-    </>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Toaster />
+    </div>
   );
 }
 
