3a6f937f8a9967c1a3bd962d34a0e212844df3c6
Ahmad Subaktagin Jabir <subaktagin.jabir16@gmail.com>
2023-03-11 10:43:39 +0600

    Loading on First Entry Added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -7,6 +8,7 @@ import Inventory from "./Pages/Inventory/Inventory";
 import AddNewItem from "./Pages/ManageInventory/AddNewItem/AddNewItem";
 import ManageInventory from "./Pages/ManageInventory/ManageInventory/ManageInventory";
 import MyItems from "./Pages/MyItems/MyItems";
+import Loading from "./Pages/Shared/Loading/Loading";
 import NotFound from "./Pages/Shared/NotFound/NotFound";
 import RequireAuth from "./Pages/SignIn/RequireAuth/RequireAuth";
 import SignIn from "./Pages/SignIn/SignIn/SignIn";
@@ -15,52 +17,69 @@ import SupplierDetails from "./Pages/Suppliers/SupplierDetails/SupplierDetails";
 import Suppliers from "./Pages/Suppliers/Suppliers/Suppliers";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/suppliers" element={<Suppliers />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/supplier/:id" element={<SupplierDetails />} />
-        <Route
-          path="/inventory/:id"
-          element={
-            <RequireAuth>
-              <Inventory />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/manageInventory"
-          element={
-            <RequireAuth>
-              <ManageInventory />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/addItem"
-          element={
-            <RequireAuth>
-              <AddNewItem />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/myItems"
-          element={
-            <RequireAuth>
-              <MyItems />
-            </RequireAuth>
-          }
-        />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+  const [loading, setLoading] = useState(false);
+  const [completed, setCompleted] = useState(undefined);
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Toaster />
-    </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+      setTimeout(() => {
+        setCompleted(true);
+      }, 2000);
+    }, 250);
+  }, []);
+  return (
+    <>
+      {!completed ? (
+        <>{loading && <Loading />}</>
+      ) : (
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/suppliers" element={<Suppliers />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/supplier/:id" element={<SupplierDetails />} />
+            <Route
+              path="/inventory/:id"
+              element={
+                <RequireAuth>
+                  <Inventory />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/manageInventory"
+              element={
+                <RequireAuth>
+                  <ManageInventory />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/addItem"
+              element={
+                <RequireAuth>
+                  <AddNewItem />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/myItems"
+              element={
+                <RequireAuth>
+                  <MyItems />
+                </RequireAuth>
+              }
+            />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Toaster />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -16,19 +16,21 @@ const Product = ({ product }) => {
       <div className="flex justify-center">
         <img
           className="p-8 max-w-xs hover:scale-110 transition duration-300 ease-in-out"
-          src={image||<Skeleton/>}
+          src={image}
           alt=""
         />
       </div>
       <div className="px-5 pb-5">
         <h5 className="text-xl font-semibold tracking-tight text-gray-900">
-          {name||<Skeleton/>}
+          {name || <Skeleton />}
         </h5>
       </div>
 
       <div className="px-5 pb-5">
         <div className="flex justify-between items-center">
-          <span className="text-3xl font-bold text-gray-900">${price||<Skeleton/>}</span>
+          <span className="text-3xl font-bold text-gray-900">
+            ${price || <Skeleton />}
+          </span>
           <button
             onClick={() => inventoryNavigate(_id)}
             className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
